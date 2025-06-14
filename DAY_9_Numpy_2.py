3ef6d2fa081ebeb1dec31b1b728299ab9e3f2a54
sabiandika <sabiandika2007@gmail.com>
2025-06-14 14:58:42 +0700

    penjumlahan matrix

--- DAY_9_Numpy_2.py
+++ DAY_9_Numpy_2.py
@@ -16,3 +16,22 @@ print(evens) # Output: [2, 4, 6]
 
 arr[arr > 3] = 0
 print ("udah diganti: ", arr) #anga yang lebih dari 3 akan beruabah jadi 0
+
+#np random
+
+random_array = np.random.rand(3, 4)  # Membuat array 2D acak dengan 3 baris dan 4 kolom
+print("Array acak:\n", random_array)
+
+random_integers = np.random.randint(0, 10, size=(2,3))  # Membuat array 2D acak dengan bilangan bulat antara 0 dan 10 
+print("Array bilangan bulat:\n", random_integers)
+
+matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+vector = np.array([1, 0, -1])
+
+result = matrix + vector 
+print("hasil penjumlahan:\n", result)  # Penjumlahan matriks dengan vektor
+# Output: [[2, 2, 2], [5, 5, 5], [8, 8, 8]] 
+
+result_kali = matrix * 2
+print("hasil perkalian:\n", result_kali)  # Perkalian matriks dengan skalar
+# Output: [[2, 4, 6], [8, 10, 12], [14, 16, 18]]
